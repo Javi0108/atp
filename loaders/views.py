ce6ad7bd45d525136687982301899e10430f9cda
Javier Garcia Dominguez <gardojavi@gmail.com>
2024-01-23 18:00:02 +0000

    mejoras en la vista

--- loaders/views.py
+++ loaders/views.py
@@ -17,15 +17,15 @@ def get_datos(request):
         if form.is_valid():
             # Aquí puedes realizar el procesamiento del archivo CSV
             # lista_archivos = [request.FILES]
-            players_rows = []
-            matches_rows = []
+            # players_rows = []
+            # matches_rows = []
             stats_rows = []
 
             for files in request.FILES:
                 print(files)
                 with open('atp-data/' + files + '.csv', 'r') as file:
                     csvreader = csv.reader(file)
-                    header = next(csvreader)
+                    # header = next(csvreader)
                     for row in csvreader:
                         if files == 'players':
                             Player.objects.get_or_create(
@@ -35,16 +35,14 @@ def get_datos(request):
                                 country=row[3],
                                 birthdate=[4],
                             )
-                            # players_rows.append(row)
                         elif files == 'matches':
-                            Player.objects.get_or_create(
-                                player_id=row[0],
-                                name=row[1],
-                                hand=row[2],
-                                country=row[3],
-                                birthdate=[4],
+                            Match.objects.get_or_create(
+                                match_id=row[0],
+                                tournament=row[1],
+                                date=row[2],
+                                round=row[3],
+                                duration=[4],
                             )
-                            matches_rows.append(row)
                         elif files == 'stats':
                             stats_rows.append(row)
 
